7945194c4355b26d93bd6c8695fb2279bb5f6e6e
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2015-12-04 18:16:00 +0500

    Rewrite the `gulp create-css-dev` task to use `refill-css-dev`

--- gulpTasks/create-css-dev.js
+++ gulpTasks/create-css-dev.js
@@ -4,8 +4,8 @@ var gulp                        = require('gulp');
 var Vinyl                       = require('vinyl');
 var process                     = require('process');
 var config                      = require('./config');
-var through2                    = require('through2');
 var reporter                    = require('./reporter');
+var refilfCssDev                = require('./refill-css-dev');
 
 /**
  * Create a dev css file
@@ -14,21 +14,6 @@ var reporter                    = require('./reporter');
 
 var taskName = 'create-css-dev';
 
-var file;
-file = new Vinyl({
-  base : 'build/',
-  path : process.cwd() + '/build/' + config.name + '.css',
-  contents : new Buffer(".myDiv{width : 100%;}")
-});
-
 gulp.task(taskName, function (done) {
-  file
-  .pipe(through2.obj(function () {
-      this.push(file);
-  }))
-  .pipe(gulp.dest('build'))
-  .pipe(through2.obj(function (file) {
-    reporter('The file `' + file.path + '` is created.', taskName, 'green');
-  }))
-  .on('end', done);
+  refilfCssDev(done);
 });
