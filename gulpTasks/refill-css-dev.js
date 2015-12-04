f8167d005bc2333831891c4ad264091f0da0f36d
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2015-12-04 18:15:19 +0500

    Make the task asynchronous

--- gulpTasks/refill-css-dev.js
+++ gulpTasks/refill-css-dev.js
@@ -7,24 +7,34 @@ var through                     = require('through');
 var util                        = require('gulp-util');
 var vinyl                       = require('vinyl');
 var glob                        = require('glob');
+var promisify                   = require('es6-promisify');
 var _                           = require('lodash');
 
 var filepath = config.build_dir + '/' + config.name + '.css';
 var scssGlob = config.scss_dir + '/**/_index.scss';
 var taskName = 'refill-css-dev';
 
-function refillCssDev () {
-  var files = glob.sync(scssGlob);
-  files = _(files).map(function (file) {
-    return file.replace(config.scss_dir + '/', '').replace('/_index.scss', '');
-  })
-  .reduce(function (memo, name) {
-    return memo += '@import url("../css/' + name + '.css");\n';
-  }, '');
+glob = promisify(glob);
+var writeFile = promisify(fs.writeFile);
 
-  fs.writeFileSync(filepath, files);
+function refillCssDev (done) {
+  var str;
+  glob(scssGlob)
+    .then(function (files) {
+      str = _(files).map(function (file) {
+        return file.replace(config.scss_dir + '/', '').replace('/_index.scss', '');
+      })
+      .reduce(function (memo, name) {
+        return memo += '@import url("../css/' + name + '.css");\n';
+      }, '');
+      return writeFile(filepath, str);
+    })
+    .then(done);
 }
 
-gulp.task(taskName, function () {
-  refillCssDev();
+
+gulp.task(taskName, function (done) {
+  refillCssDev(done);
 });
+
+module.exports = refillCssDev;
