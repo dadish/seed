0b4003bf56c1012f800731f0c299409c26c1cb61
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2015-12-08 13:19:17 +0500

    Implement `gulp create-css-build` task

--- gulpTasks/remove-css-build.js
+++ gulpTasks/remove-css-build.js
@@ -1,14 +1,21 @@
 var gulp                        = require('gulp');
 var del                         = require('del');
 var config                      = require('./config');
+var reporter                    = require('./reporter');
+
+var taskName = 'remove-css-build';
 
 /**
  * Remove the build css file.
  * The filename for css build is {name}.{hash}.css
  * We match it with {name}.*.css
  */
-gulp.task('remove-css-build', function (done) {
+gulp.task(taskName, function (done) {
   var filename;
-  filename = config.buildDir + config.name + '.*.css';
-  del([filename], done);
+  filename = config.buildDir + '/' + config.name + '-*.css';
+  del([filename])
+    .then(function (path) {
+      reporter('Removed file at `' + path + '`', taskName, 'cyan');
+      done();
+    });    
 });
