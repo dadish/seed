df25054e51f3c8ee96afbde00f5195be0a7ffedd
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-22 10:53:48 +0600

    Add compact descriptions of the action creators.

--- src/containers/ToRead/actions.js
+++ src/containers/ToRead/actions.js
@@ -13,14 +13,57 @@ import {
 } from './constants';
 import createActionCreator  from 'utils/createActionCreator';
 
+/**
+ * Create action with type CHANGE_SEARCH_TXT
+ */
 export const changeSearchTxt = createActionCreator(CHANGE_SEARCH_TXT);
+
+/**
+ * Create action with type SUGGESTIONS_LOOKUP_START
+ */
 export const suggestionsLookupStart = createActionCreator(SUGGESTIONS_LOOKUP_START);
+
+/**
+ * Create action with type SUGGESTIONS_LOOKUP_END
+ */
 export const suggestionsLookupEnd = createActionCreator(SUGGESTIONS_LOOKUP_END);
+
+/**
+ * Create action with type SUGGESTIONS_LOOKUP_CANCEL
+ */
 export const suggestionsLookupCancel = createActionCreator(SUGGESTIONS_LOOKUP_CANCEL);
+
+/**
+ * Create action with type SUGGESTIONS_LOOKUP_FAIL
+ */
 export const suggestionsLookupFail = createActionCreator(SUGGESTIONS_LOOKUP_FAIL);
+
+/**
+ * Create action with type SHOW_SUGGESTIONS
+ */
 export const showSuggestions = createActionCreator(SHOW_SUGGESTIONS);
+
+/**
+ * Create action with type SELECT_SUGGESTION
+ */
 export const selectSuggestion = createActionCreator(SELECT_SUGGESTION);
+
+/**
+ * Create action with type ADD_SUGGESTION
+ */
 export const addSuggestion = createActionCreator(ADD_SUGGESTION);
+
+/**
+ * Create action with type DELETE_SUGGESTION
+ */
 export const deleteSuggestion = createActionCreator(DELETE_SUGGESTION);
+
+/**
+ * Create action with type MARK_SUGGESTION_READ
+ */
 export const markSuggestionRead = createActionCreator(MARK_SUGGESTION_READ);
+
+/**
+ * Create action with type MARK_SUGGESTION_UNREAD
+ */
 export const markSuggestionUnread = createActionCreator(MARK_SUGGESTION_UNREAD);
