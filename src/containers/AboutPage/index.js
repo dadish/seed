8d37a4c2fbde864a88b666a341d7a65f1c42cc59
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-23 17:00:57 +0600

    Add some explanation text.

--- src/containers/AboutPage/index.js
+++ src/containers/AboutPage/index.js
@@ -1,8 +1,38 @@
 import React from 'react';
 
 const About = () => (
-  <div>
-    <h1>About Page!</h1>
+  <div className="pg-w">
+    <h1>About Seed</h1>
+    <p>
+      This boilerplate is an extension of the <a href="https://github.com/facebookincubator/create-react-app">create-react-app</a>.
+      I basically created a CRA app and added libraries that I use a lot. The libraries included are:
+    </p>
+    <p>Dependencies:</p>
+    <ul>
+    <li>immutable</li>
+    <li>lodash</li>
+    <li>react</li>
+    <li>react-dom</li>
+    <li>react-redux</li>
+    <li>react-router</li>
+    <li>react-router-redux</li>
+    <li>react-router-scroll</li>
+    <li>redux</li>
+    <li>redux-immutable</li>
+    <li>redux-observable</li>
+    <li>reselect</li>
+    <li>rxjs</li>
+    </ul>
+
+    <p>Dev Dependencies:</p>
+    <ul>
+      <li>enzyme</li>
+      <li>gh-pages</li>
+      <li>react-addons-test-utils</li>
+      <li>invariant</li>
+      <li>warning</li>
+      <li>react-scripts</li>
+    </ul>
   </div>
 );
 
--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Home = () => (
-  <div>
-    <h1>Home Page</h1>
+  <div className="pg-w">
+    <h1>Seed</h1>
+    <p>
+      This is <a href="https://github.com/dadish" target="_blank">my</a> personal boilerplate repository for 
+      creating <a href="https://facebook.github.io/react/">React</a> applications.
+    </p>
   </div>
 );
 
