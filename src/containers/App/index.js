71dc87205a457db96bdc9224279ce0bc7175c50a
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-23 13:55:56 +0600

    Implement basic support for hot module reload.

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,12 +1,55 @@
 import React from 'react';
 import Menu from 'components/Menu';
+import { Provider } from 'react-redux';
+import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { useScroll } from 'react-router-scroll';
+import { fromJS } from 'immutable';
+import configureStore from 'setup/store';
+import { makeSelectLocationState } from 'setup/selectors';
+import createRoutes from 'setup/routes';
 import style from './style';
+import 'setup/index.css';
 
-const App = ({ children }) => (
+export const App = ({ children }) => (
   <div style={style.main}>
     <Menu />
     {children}
   </div>
 );
 
-export default App;
+// Create redux store with history
+// this uses the singleton browserHistory provided by react-router
+// Optionally, this could be changed to leverage a created history
+// e.g. `const browserHistory = useRouterHistory(createbrowserHistory)();`
+const initialState = fromJS({});
+const store = configureStore(initialState, browserHistory);
+
+// Sync history and store, as the react-router-redux reducer
+// is under the non-default key ("routing"), selectLocationState
+// must be provided for resolving how to retrieve the "route" in the state
+const history = syncHistoryWithStore(browserHistory, store, {
+  selectLocationState: makeSelectLocationState(),
+});
+
+// Set up the router, wrapping all Routes in the App component
+const rootRoute = {
+  component: App,
+  childRoutes: createRoutes(store),
+};
+
+const AppWrapper = () => (
+  <Provider store={store}>
+    <Router
+      history={history}
+      routes={rootRoute}
+      render={
+        // Scroll to top when going to a new page, imitating default browser
+        // behaviour
+        applyRouterMiddleware(useScroll())
+      }
+    />
+  </Provider>
+);
+
+export default AppWrapper;
--- src/index.js
+++ src/index.js
@@ -1,49 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { useScroll } from 'react-router-scroll';
-import { fromJS } from 'immutable';
-
-import 'setup/index.css';
-
-import configureStore from 'setup/store';
-import { makeSelectLocationState } from 'setup/selectors';
-import createRoutes from 'setup/routes';
 import App from 'containers/App';
 
-// Create redux store with history
-// this uses the singleton browserHistory provided by react-router
-// Optionally, this could be changed to leverage a created history
-// e.g. `const browserHistory = useRouterHistory(createbrowserHistory)();`
-const initialState = fromJS({});
-const store = configureStore(initialState, browserHistory);
-
-// Sync history and store, as the react-router-redux reducer
-// is under the non-default key ("routing"), selectLocationState
-// must be provided for resolving how to retrieve the "route" in the state
-const history = syncHistoryWithStore(browserHistory, store, {
-  selectLocationState: makeSelectLocationState(),
-});
-
-// Set up the router, wrapping all Routes in the App component
-const rootRoute = {
-  component: App,
-  childRoutes: createRoutes(store),
-};
+const rootEl = document.getElementById('root');
 
 ReactDOM.render(
-  <Provider store={store}>
-    <Router
-      history={history}
-      routes={rootRoute}
-      render={
-        // Scroll to top when going to a new page, imitating default browser
-        // behaviour
-        applyRouterMiddleware(useScroll())
-      }
-    />
-  </Provider>,
-  document.getElementById('root')
+  <App />,
+  rootEl
 );
+
+if (module.hot) {
+  module.hot.accept('containers/App', () => {
+    const NextApp = require('containers/App').default;
+    ReactDOM.render(
+      <NextApp />,
+      rootEl
+    );
+  });
+}
