4ea6b6c573c580f16be2ce535011bb9d4512acd0
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-24 11:28:12 +0600

    Wrap travis-badge into travis link.

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -4,7 +4,7 @@ import './style.css';
 
 const Menu = () => (
   <div className="menu-w">
-    <img className="travis-badge" src="https://api.travis-ci.org/dadish/seed.svg?branch=master" alt="Travis-CI"/>
+    <a className="travis-badge" href="https://travis-ci.org/dadish/seed" target="_blank"><img src="https://api.travis-ci.org/dadish/seed.svg?branch=master" alt="Travis-CI"/></a>
     <ul className="menu-l">
       <li className="menu-i"><Link className="menu-ia" activeClassName="menu-ia--active" to="/">Home</Link></li>
       <li className="menu-i"><Link className="menu-ia" activeClassName="menu-ia--active" to="/about">About</Link></li>
