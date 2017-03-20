583971068f5b0a7b22bb1c9240e47b4f01c4b607
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-20 17:30:32 +0600

    Add not found page

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
 import './style.css';
 import Menu from 'components/Menu';
 import Home from 'containers/HomePage/async';
 import About from 'containers/AboutPage/async';
 import Search from 'containers/GoSearch/async';
+import NotFound from 'containers/NotFoundPage/async';
 
 export const App = ({ children }) => (
   <Router>
     <div className="app-w">
       <Menu />
+      <Switch>
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/search" component={Search} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   </Router>
 );
